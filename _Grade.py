b29900d05c1672562c432b945d105c4e4211c4d1
Nanhisinghmar <kajalsinghmar80@gmail.com>
2026-06-06 19:12:16 +0530

    Add files via upload

---
+++ _Grade.py
@@ -0,0 +1,19 @@
+marks = int(input("Enter marks: "))
+
+if marks >= 90:
+    print("Grade A+")
+
+elif marks >= 80:
+    print("Grade A")
+
+elif marks >= 70:
+    print("Grade B")
+
+elif marks >= 60:
+    print("Grade C")
+
+elif marks >= 40:
+    print("Grade D")
+
+else:
+    print("Fail")
